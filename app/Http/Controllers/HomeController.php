87b91f943dce64b50f313ba02d09187329bfadd2
kurtjohn21 <kjmojado21@gmail.com>
2024-01-26 19:01:19 +0800

    added suggested and filter

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 class HomeController extends Controller
 {
     /**
@@ -25,7 +27,29 @@ class HomeController extends Controller
     public function index()
     {
         $all_posts = $this->post->latest()->get();
+        $home_posts = [];
+        $suggested_users = $this->suggested_users();
+        foreach($all_posts as $post){
+            if($post->user->isFollowed() || $post->user_id == Auth::user()->id){
+                $home_posts[] = $post;
+            }
+        }
         return view('users.home')
-                ->with('all_posts',$all_posts);
+                ->with('all_posts',$home_posts)
+                ->with('suggested_users',$suggested_users);
     }
+    public function suggested_users(){
+        $all_users = User::all()->except(Auth::user()->id);
+        $suggested_users = [];
+
+        foreach($all_users as $user){
+            if(!$user->isFollowed()){
+                $suggested_users[] = $user;
+            }
+        }
+
+        return $suggested_users;
+    }
+    // filter the post
+    // show suggested users
 }
--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title','Home')
+@section('title', 'Home')
 
 @section('content')
     <div class="row">
@@ -18,12 +18,50 @@
                 <div class="text-center">
                     <h2>Share Photos</h2>
                     <p class="text-muted">When you share photos, they'll appear on your profile</p>
-                    <a href="{{route('post.create')}}" class="text-decoration-none">Share your first photo</a>
+                    <a href="{{ route('post.create') }}" class="text-decoration-none">Share your first photo</a>
                 </div>
             @endforelse
         </div>
-        <div class="col-4 bg-secondary">
-            PROFILE OVERVIEW + SUGGESTIONS
+        <div class="col-4 ">
+            <div class="row align-items-center mb-5 bg-white shadow-sm rounded-3 py-3">
+                <div class="col-auto">
+                    <a href="{{ route('profile.show', Auth::user()->id) }}">
+                        @if (Auth::user()->avatar)
+                            <img src="{{ Auth::user()->avatar }}" alt="" class="rounded-circle avatar-md">
+                        @else
+                            <i class="fa-solid fa-circle-user text-secondary icon-md"></i>
+                        @endif
+                    </a>
+                </div>
+                <div class="col ps-0">
+                    <a href="{{ route('profile.show', Auth::user()->id) }}"
+                        class="text-decoration-none text-dark fw-bold">{{ Auth::user()->name }}</a>
+                    <p class="text-muted mb-0">{{ Auth::user()->email }}</p>
+                </div>
+            </div>
+            @foreach ($suggested_users as $user)
+                <div class="row align-items-center mb-3">
+                    <div class="col-auto">
+                        @if ($user->avatar)
+                            <img src="{{ $user->avatar }}" alt="" class="rounded-circle avatar-sm">
+                        @else
+                            <i class="fa-solid fa-circle-user text-secondary"></i>
+                        @endif
+                    </div>
+                    <div class="col ps-0 text-truncate">
+                        <a href="{{ route('profile.show', $user->id) }}"
+                            class="text-secondary text-dark fw-bold text-decoration-none">{{ $user->name }}</a>
+                    </div>
+                    <div class="col-auto">
+                        <form action="{{ route('follow.store', $user->id) }}" method="post">
+                            @csrf
+
+                            <button type="submit" class="border-0 bg-transparent p-0 text-primary btn-sm">Follow</button>
+
+                        </form>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
